066c1511112fb461e67c2ecddc1585e9a9bb81eb
ranjithdronadula <dronadularanjith@gmail.com>
2025-05-30 21:26:05 +0530

    create instances

--- roboshop.sh
+++ roboshop.sh
@@ -1,28 +1,26 @@
-#i/bin/bash
+#!/bin/bash
 
-AMI="ami-09c813fb71547fc4f"
-SG_ID="sg-0004821aded493804"
-INSTANCES=("mongodb" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontenf")
-ZONE_ID="Z08241221PV7NRI1REOJQ"
-DOMAIN_NAME="ranjithdaws.site"
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-0004821aded493804" # replace with your SG ID
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
+ZONE_ID="Z08241221PV7NRI1REOJQ" # replace with your ZONE ID
+DOMAIN_NAME="ranjithdaws.site" # replace with your domain
 
-
-#for instances ${instances[@]}
-
+#for instance in ${INSTANCES[@]}
 for instance in $@
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0004821aded493804 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
-    if [ $instance != frontend ]
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids "sg-0004821aded493804" --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    if [ $instance != "frontend" ]
     then
-    IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
         RECORD_NAME="$instance.$DOMAIN_NAME"
     else
-    IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
         RECORD_NAME="$DOMAIN_NAME"
     fi
-        echo "$instance IP of : $IP"
+    echo "$instance IP address: $IP"
 
-        aws route53 change-resource-record-sets \
+    aws route53 change-resource-record-sets \
     --hosted-zone-id $ZONE_ID \
     --change-batch '
     {
