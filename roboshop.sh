12ddc0373b001d1d97c4ea1091118bd0b43598d3
ranjithdronadula <dronadularanjith@gmail.com>
2025-05-31 15:56:28 +0530

    instances creation

--- roboshop.sh
+++ roboshop.sh
@@ -9,21 +9,19 @@ ZONE_ID="Z08241221PV7NRI1REOJQ" # replace with your ZONE ID
 DOMAIN_NAME="ranjithdaws.site" # replace with your domain
  
 
- echo $AMI_ID $SG_ID $INSTANCES
- 
-# #for instance in ${INSTANCES[@]}
-# for instance in $@
-# do
-#     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0004821aded493804 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
-#     if [ $instance != "frontend" ]
-#     then
-#         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
-#         RECORD_NAME="$instance.$DOMAIN_NAME"
-#     else
-#         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
-#         RECORD_NAME="$DOMAIN_NAME"
-#     fi
-#     echo "$instance IP address: $IP"
+#for instance in ${INSTANCES[@]}
+for instance in $@
+do
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0004821aded493804 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    if [ $instance != "frontend" ]
+    then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME"
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+        RECORD_NAME="$DOMAIN_NAME"
+    fi
+    echo "$instance IP address: $IP"
 
 #     aws route53 change-resource-record-sets \
 #     --hosted-zone-id $ZONE_ID \
