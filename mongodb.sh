497a8d7dda32fcc994d4caf0551e2d23ab9d779c
ranjithdronadula <dronadularanjith@gmail.com>
2025-06-02 16:41:53 +0530

    mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -48,8 +48,8 @@ then
         systemctl start mongod &>>$LOG_FILE 
         VOLIDATE $? "Starting MongoDB"
 |
-        sed -i 's/27.0.0.1/0.0.0.0/g' /etc/mongod.conf
+        sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
         VALIDATE $? "Editing MongoDB Repo file to Remote Conections"
 
         systemctl restart mongod &>>$LOG_FILE
-        Volidate $? " REstating MongoDB"
+        Volidate $? "Restating MongoDB"
