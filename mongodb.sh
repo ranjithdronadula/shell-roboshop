34bd6330711025d7d6530a36113740ce27ec3189
ranjithdronadula <dronadularanjith@gmail.com>
2025-06-02 16:10:44 +0530

    mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -10,23 +10,23 @@ LOG_FOLDER="/var/log/roboshop.log"
 SCRIPT_NAME="$(echo $0 | cut -d "." f1)"
 LOG_FILES="$LOG_FOLDER/$SCRIPT_NAME.log"
 
-echo "This  script started excuting at :: $(date)"
+echo "$R This  script started excuting at :: $(date) $N" 
 
 
 if [ $USERID -ne 0 ]
 then
-    echo -e "ERROR  ::  plese run this script root acces"
+    echo -e "$R ERROR  ::  plese run this script root acces $N"
     exit 1
  else
-     echo -e "this script running with root access nothing to do" 
+     echo -e "$G this script running with root access nothing to do $N" 
     fi 
 
   VOLIDATE(){
             if [ $1 -eq 0 ]
             then 
-                echo -e "$2 ... IS SUCCSESS"
+                echo -e "$2 ... IS $Y SUCCSESS $N"
             else
-                echo -e "$2.....is FAILURE"
+                echo -e "$2.....is $R FAILURE $N "
                  exit 1
            
            fi
