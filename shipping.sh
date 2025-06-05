8fe51a4261410e825c8690bb9b29d75fc6ca60a6
ranjithdronadula <dronadularanjith@gmail.com>
2025-06-05 18:07:06 +0530

    shipping creation

--- shipping.sh
+++ shipping.sh
@@ -1,29 +1,26 @@
 #!/bin/bash
 
 START_TIME=$(date +%s)
-USER_ID=(id -u)
-
+USERID=$(id -u)
 R="\e[31m"
-G="\e[33m"
+G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-
-LOG_FOLDER="/var/log/roboshop.log"
-SCRIPT_NAME="$(echo $0 | cut -d "." f1)"
-LOG_FILE="$LOG_FOLDWE/$SCRIPT_NMAE.LOG"
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 SCRIPT_DIR=$PWD
 
-mkdir -p $LOG_FOLDER
+mkdir -p $LOGS_FOLDER
 echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
 # check the user has root priveleges or not
-
 if [ $USERID -ne 0 ]
 then
-    echo -e "$G ERROR... Please run this Script root access$N" | tee -a $LOG_FILE
-    exit 1
- else   
-    echo -e "$Y Yor are running with root aceess..Nothing to do$N" | tee -a $LOG_FILE
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
+else
+    echo "You are running with root access" | tee -a $LOG_FILE
 fi
 
 echo "Please enter root password to setup"
@@ -31,62 +28,59 @@ read -s MYSQL_ROOT_PASSWORD
 
 # validate functions takes input as exit status, what command they tried to install
 VALIDATE(){
-        if [ $1 -eq 0 ]
-        then
-            echo -e "$2...... is $G SUCCESS $N" | tee -a $LOG_FILE
-         else
-            echo -e "$2.......is $R FAILURE $N" | tee -a $LOG_FILE
-            exit 1
-        fi 
-        } 
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
 
 dnf install maven -y &>>$LOG_FILE
-VALIDATE $? "Installing Maven Service"
+VALIDATE $? "Installing Maven and Java"
 
-id roboshop
-if [ $? -ne ]
+id roboshop &>>$LOG_FILE
+if [ $? -ne 0 ]
 then
-     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
-     VALIDATE $? "Creating roboshop system user"
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+    VALIDATE $? "Creating roboshop system user"
 else
-    echo -e ""System user roboshop already created ... $Y SKIPPING $N"
+    echo -e "System user roboshop already created ... $Y SKIPPING $N"
 fi
 
-mkdir -p /app
-VALIDATE $? "Creating app Directory"
+mkdir -p /app 
+VALIDATE $? "Creating app directory"
 
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip &>>$LOG_FILE  
+curl -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip &>>$LOG_FILE
 VALIDATE $? "Downloading shipping"
 
 rm -rf /app/*
 cd /app 
-
 unzip /tmp/shipping.zip &>>$LOG_FILE
-VALIDATE $? "Unzipping shipping files"
+VALIDATE $? "unzipping shipping"
 
-
-mvn clean package &>>$LOG_FILE
+mvn clean package  &>>$LOG_FILE
 VALIDATE $? "Packaging the shipping application"
 
-mv target/shipping-1.0.jar shipping.jar &>>$LOG_FILE
+mv target/shipping-1.0.jar shipping.jar  &>>$LOG_FILE
 VALIDATE $? "Moving and renaming Jar file"
 
-
-cp SCRIPT_DIR/shipping.services /etc/systemd/system/shipping.service 
+cp $SCRIPT_DIR/shipping.services /etc/systemd/system/shipping.service
 
 systemctl daemon-reload &>>$LOG_FILE
-VALIDATE $? "Daemon reload"
+VALIDATE $? "Daemon Realod"
 
-systemctl enable shipping &>>$LOG_FILE
+systemctl enable shipping  &>>$LOG_FILE
 VALIDATE $? "Enabling Shipping"
 
 systemctl start shipping &>>$LOG_FILE
 VALIDATE $? "Starting Shipping"
 
-dnf install mysql -y &>>$LOG_FILE
-VALIDATE $? "Installing Mysql"
+dnf install mysql -y  &>>$LOG_FILE
+VALIDATE $? "Install MySQL"
 
-mysql -h mysql.ranjithdaws.site -u root -p$MYSQL_ROOT_PASSWORD -e 'use cities' &>>$LOG_FILE
+mysql -h mysql.daws84s.site -u root -p$MYSQL_ROOT_PASSWORD -e 'use cities' &>>$LOG_FILE
 if [ $? -ne 0 ]
 then
     mysql -h mysql.ranjithdaws.site -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/schema.sql &>>$LOG_FILE
@@ -96,8 +90,9 @@ then
 else
     echo -e "Data is already loaded into MySQL ... $Y SKIPPING $N"
 fi
+
 systemctl restart shipping &>>$LOG_FILE
-VALIDATE $? "Restarting Shipping"
+VALIDATE $? "Restart shipping"
 
 END_TIME=$(date +%s)
 TOTAL_TIME=$(( $END_TIME - $START_TIME ))
