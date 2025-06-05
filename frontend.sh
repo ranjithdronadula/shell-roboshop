79a24241079767c1da39e47e58407b04257e05c2
ranjithdronadula <dronadularanjith@gmail.com>
2025-06-05 15:58:52 +0530

    frontend creation

--- frontend.sh
+++ frontend.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+START_TIME=$(date +%s)
 USERID=(id -u)
 R="\e[31m"
 G="\e[32m"
@@ -67,3 +68,9 @@ VALIDATE $? "Copying nginx.conf"
 
 systemctl restart nginx 
 VALIDATE $? "Restarting nginx"
+
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+
+echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
