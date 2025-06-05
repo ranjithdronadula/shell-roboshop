d9c0c3462ece5cb6b2951379b2cbf8f41cf5526c
ranjithdronadula <dronadularanjith@gmail.com>
2025-06-05 15:55:33 +0530

    catalogue creation

--- catalogue1.sh
+++ catalogue1.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+START_TIME=$(date +%s)
 USERID=(id -u)
 R="\e[31m"
 G="\e[32m"
@@ -89,3 +90,8 @@ then
 else
     echo -e "Data is already loades..$Y SKIPPING $N"
 fi    
+
+   END_TIME=$(date +%s)
+   TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+
+   echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
