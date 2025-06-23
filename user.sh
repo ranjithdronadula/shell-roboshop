b040d3efc53570cca0c683dd241e990750ab1250
ranjithdronadula <dronadularanjith@gmail.com>
2025-06-23 15:11:04 +0530

    user creation

--- user.sh
+++ user.sh
@@ -77,8 +77,7 @@ systemctl enable user  &>>$LOG_FILE
 systemctl start user
 VALIDATE $? "Starting user"
 
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
 
-   END_TIME=$(date +%s)
-   TOTAL_TIME=$(( $END_TIME - $START_TIME ))
-
-   echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
+echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
